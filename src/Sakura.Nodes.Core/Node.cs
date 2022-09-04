2399e9d2d00392bcd0231e46bd5757917947b316
Wesley Brown <wesleybrown@wesleybrown.net>
2022-09-04 18:34:08 -0400

    Update Node
    Creating a node will not support a null entity ID and the string of a
    Node will contain its entity ID.

--- src/Sakura.Nodes.Core/Node.cs
+++ src/Sakura.Nodes.Core/Node.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Sakura.Nodes.Core
 {
@@ -7,16 +9,83 @@ namespace Sakura.Nodes.Core
     /// </summary>
     public sealed class Node
     {
+        /// <summary>
+        ///     Try and create a Node with a given entity ID and resource item
+        ///     ID.
+        /// </summary>
+        /// <param name="entity">
+        ///     The ID of the entity that will be represented by the created
+        ///     Node, if successful.
+        /// </param>
+        /// <param name="ID">
+        ///     The ID of the resource item that the created Node will have, if
+        ///     successful.
+        /// </param>
+        /// <param name="node">
+        ///     The created Node, if there is one.
+        /// </param>
+        /// <returns>
+        ///     A list of errors encountered while trying to create a Node, if
+        ///     unsuccessful. An empty list if successful.
+        /// </returns>
         public static string[] TryParse(
             string entity,
-            string ID,
+            string resourceItemID,
             out Node node)
         {
-            node = null;
             var errors = new List<string>();
-            if (ID == null)
-                errors.Add("The resource ID of a node cannot be null.");
+            if (entity == null)
+                errors.Add("The entity ID of a node cannot be null.");
+            if (resourceItemID == null)
+                errors.Add("The resource item ID of a node cannot be null.");
+            if (errors.Count > 0)
+                node = null;
+            else
+                node = new Node(
+                    entity,
+                    resourceItemID);
             return errors.ToArray();
+        }
+
+        private Node(
+            string entity,
+            string resourceItemID)
+        {
+            Debug.Assert(entity != null);
+            try
+            {
+                this.entity = new Guid(entity);
+            }
+            catch (FormatException)
+            {
+                throw new NotImplementedException();
+            }
+            catch (OverflowException)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private readonly Guid entity;
+
+        /// <summary>
+        ///     The entity that this Node represents.
+        /// </summary>
+        public Guid Entity
+        {
+            get
+            {
+                return entity;
+            }
+        }
+
+        /// <summary>
+        ///     The string representation of this Node.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Entity={Entity}";
         }
     }
 }
--- tests/Sakura.Nodes.Core.Tests/NodeSpec.cs
+++ tests/Sakura.Nodes.Core.Tests/NodeSpec.cs
@@ -7,6 +7,19 @@ namespace NodeSpec
     public class Creating
     {
         [Test]
+        public void Does_Not_Support_A_Null_Entity_ID()
+        {
+            string entity = null;
+            var resourceItemID = "resource";
+            var errors = Node.TryParse(
+                entity,
+                resourceItemID,
+                out var node);
+            Assert.IsNotEmpty(errors);
+            Assert.IsNull(node);
+        }
+
+        [Test]
         public void Does_Not_Support_A_Null_Resource_Item_ID()
         {
             var entity = "945186bf-ba80-43bc-b41b-6aa542b88c63";
@@ -17,6 +30,26 @@ namespace NodeSpec
                 out var node);
             Assert.IsNotEmpty(errors);
             Assert.IsNull(node);
+        }
+    }
+
+    [TestFixture]
+    public class The_String_Representation
+    {
+        [Test]
+        public void Includes_The_Entity_ID()
+        {
+            var entity = "734856d2-e288-4c2f-9862-e62c5ccc69c3";
+            var resourceItemID = "resource";
+            var errors = Node.TryParse(
+                entity,
+                resourceItemID,
+                out var node);
+            Assert.IsEmpty(errors);
+            Assert.IsNotNull(node);
+            StringAssert.Contains(
+                entity,
+                node.ToString());
         }
     }
 }
